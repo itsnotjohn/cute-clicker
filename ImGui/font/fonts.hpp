09c7b82eab03de5967e8b7ca61a603332f3a15e0
joão <42720127+itsnotjohn@users.noreply.github.com>
2024-04-14 15:22:41 -0300

    Add files via upload

---
+++ ImGui/font/fonts.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+class fonts {
+public:
+	//Default Cute Font
+	const unsigned int getFontSize( );
+	const unsigned int *getFontBytes( );
+};
+
+class font_icon {
+public:
+	//NotoSans-Regular.ttf
+	const unsigned int getFontSize( );
+	const unsigned int *getFontBytes( );
+};
